0aed571600c8151bb86a99150fabb1f487d23c2a
weiyiming <1584879727@qq.com>
2020-08-30 11:33:08 +0800

    新增接口文档

--- book/js/page2.js
+++ book/js/page2.js
@@ -1,15 +1,15 @@
 var app = new Vue({
   el: "#body",
   data: {
-    b_type:[],
+    b_type: [],
     b_arr: [],
-
+    b_keyword: "",
   },
   methods: {
     getValue: function () {
       var that = this;
       axios.get("../json/page2.json").then(
-        function (response) {b_type:
+        function (response) {
           console.log(response.data.book_data);
           that.b_arr = response.data.book_data;
         },
@@ -18,46 +18,28 @@ var app = new Vue({
     },
 
     select: function () {
-      this.newsArr = [];
-      if (this.sArr.length < this.temp.length) {
-        this.sArr = this.temp;
-      }
-      if (this.sName != "") {
-        for (var i = 0; i < this.sArr.length; i++) {
-          if (this.sArr[i].sName === this.sName) {
-            this.newsArr.push(this.sArr[i]);
-          }
-        }
-        if (this.newsArr.length !== 0) {
-          this.temp = this.sArr;
-          this.sArr = this.newsArr;
-          this.newsArr = this.temp;
-          // this.sArr = this.temp;
-        } else {
-          alert("查询无果！");
-        }
-      }
-      // } else this.sArr = this.temp;
-      console.log(this.sArr);
-      this.sortByAdd();
+      var that = this;
+      axios
+        .get("", {
+          params: {
+            // 通过作者或者书名查询
+            keyword: that.b_keyword,
+          },
+        })
+        .then(function (request) {})
+        .catch(function (err) {
+          console.log("error");
+          console.log(that.b_keyword);
+        });
     },
-
-    addStu: function () {
-      if (this.addArr.sC == null) {
-        alert("添加失败，请输入正确的信息！");
-        this.addArr = [
-          { sName: "", sC: 0, sM: 0, sE: 0, sP: 0, sCm: 0, sB: 0, sAdd: 0 },
-        ];
-      } else {
-        console.log(this.addArr);
-        this.sArr.push(this.addArr);
-        this.add();
-        this.sortByAdd();
-        this.$forceUpdate();
-        this.addArr = [
-          { sName: "", sC: 0, sM: 0, sE: 0, sP: 0, sCm: 0, sB: 0, sAdd: 0 },
-        ];
-      }
+    borrow: function (b_id) {
+      var that = this;
+      axios
+        .post("", {
+          book_id: b_id,
+          csrfmiddlewaretoken: "{{csrf_token}}",
+        })
+        .then(function (request) {});
     },
   },
   mounted() {
